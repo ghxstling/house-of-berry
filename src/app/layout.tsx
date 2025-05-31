88c9118a8632db89d9ac8b9765b690d3b1ff73b6
ghxstling <dcho282@aucklanduni.ac.nz>
2025-05-31 19:26:10 +1200

    feat(Footer): add Footer component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import type { Metadata } from "next";
 import "./globals.css";
+
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "House of Berry",
@@ -21,6 +23,7 @@ export default function RootLayout({
         <main className="flex flex-col items-center min-h-screen">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
