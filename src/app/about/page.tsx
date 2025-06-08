5870b676138522bf03c4f9408aa0a28a16e4d7ca
ghxstling <dcho282@aucklanduni.ac.nz>
2025-06-08 22:44:22 +1200

    refactor: update layout and improve text structure

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,38 +4,42 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="w-full h-[100vh] relative">
+    <section className="w-full h-[60rem] relative">
       {/* Background Image */}
-      <Image 
+      <Image
         src={jacqueline}
-        alt="bg-img-about-page"
+        alt="Photo of Jacqueline"
         fill
-        className="object-cover object-center"
+        className="object-cover"
       />
 
       {/* Text Overlay */}
-      <div className="absolute inset-0 flex items-center justify-end">
-        <div className=" max-w-md text-center m-40">
-          <h1 className="text-6xl font-bold text-gray-800 mb-16">About Us</h1>
-          <div className="text-center">
-            <p className="text-lg text-gray-900 mb-4">
+      <div className="absolute inset-0 -top-[35%] right-30 flex items-center justify-end">
+        <div className=" w-[36rem] text-center">
+          <h1 className="text-6xl font-bold text-gray-800 mb-8">About Us</h1>
+          <div className="text-center text-lg text-gray-900 space-y-4">
+            <p>
               A fun experience. An act of creativity. A moment of indulgence.
             </p>
-            
-            <p className="text-lg text-gray-900 mb-4">
-              House of Berry was born from a simple love: the daily ritual of matcha. But not just any matcha—matcha made fun, fresh, and unexpected. Wanting to break away from the ordinary, we set out to reimagine what a morning pick-me-up could be.
+            <p>
+              House of Berry was born from a simple love: the daily ritual of
+              matcha. But not just any matcha—matcha made fun, fresh, and
+              unexpected. Wanting to break away from the ordinary, we set out to
+              reimagine what a morning pick-me-up could be.
             </p>
-            
-            <p className="text-lg text-gray-900 mb-4">
-              Our drinks are designed to be as exciting as they are refreshing, from iced matcha whisked to perfection before your eyes to rich, smooth iced lattes crafted from bold flavours.
+            <p>
+              Our drinks are designed to be as exciting as they are refreshing,
+              from iced matcha whisked to perfection before your eyes to rich,
+              smooth iced lattes crafted from bold flavours.
             </p>
-            
-            <p className="text-lg text-gray-900">
-              House of Berry isn't just about coffee or matcha. It's about turning everyday moments into something special - one sip at a time.
+            <p>
+              House of Berry isn&apos;t just about coffee or matcha. It&apos;s
+              about turning everyday moments into something special - one sip at
+              a time.
             </p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
