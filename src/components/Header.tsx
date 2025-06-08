54da9344033e7a2f64236cb9dbbb9861f1dc5d4e
Dylan Choy <dylan.choy21@gmail.com>
2025-06-08 22:31:29 +1200

    Header (#13)
    * feat: Added header with cropped version of logo
    
    * fix: adjust styles and prettier linting
    
    * fix: adjust sizing
    
    ---------
    
    Co-authored-by: Dylan Lim <dylanlim2035@gmail.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-green-200">
-      <p>
-        placeholder for header (<code>/components/Header.tsx</code>)
-      </p>
+    <header className="flex items-center justify-between py-4 px-12 bg-[#fff0fc] shadow-sm">
+      <div className="relative w-30 h-16">
+        <Image
+          src="/header_logo.jpg"
+          alt="Logo"
+          fill
+          style={{ objectFit: "contain" }}
+          priority
+        />
+      </div>
+
+      <nav className="flex space-x-8 text-gray-800 font-medium">
+        <Link href="/" className="hover:text-blue-600 ">
+          HOME
+        </Link>
+        <Link href="/about" className="hover:text-blue-600">
+          ABOUT US
+        </Link>
+        <Link href="/offerings" className="hover:text-blue-600">
+          OFFERINGS
+        </Link>
+        <Link href="/faqs" className="hover:text-blue-600">
+          FAQS
+        </Link>
+        <Link href="/contact" className="hover:text-blue-600">
+          CONTACT
+        </Link>
+      </nav>
     </header>
   );
 }
