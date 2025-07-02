e24e6b99cd43c2a0bd33d94fe897148849b0f740
ghxstling <dcho282@aucklanduni.ac.nz>
2025-07-03 00:38:05 +1200

    refactor: improve component style and structure, simplify code to make it more readable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,209 +1,199 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import React from "react";
-import clsx from "clsx";
-import { Playfair_Display } from 'next/font/google';
+import Link from "next/link";
 
-const playfair = Playfair_Display({
-  subsets: ['latin'],
-  weight: ['400', '900'],
-  style: ['normal', 'italic']
-});
+import { Button } from "@/components/ui/button";
 
+const heroCollage = [
+  [
+    {
+      src: "/homepage/1.png",
+      alt: "Lots of Love Matcha",
+    },
+    {
+      src: "/homepage/2.png",
+      alt: "House of Berry Market Stand",
+    },
+    {
+      src: "/homepage/3.png",
+      alt: "Love you so Matcha",
+    },
+    {
+      src: "/homepage/4.png",
+      alt: "Pouring whisked matcha into cup",
+    },
+  ],
+  [
+    {
+      src: "/homepage/8.png",
+      alt: "House of Berry setup",
+    },
+    {
+      src: "/homepage/7.png",
+      alt: "Whisked just for you, matcha bar on the go",
+    },
+    {
+      src: "/homepage/6.png",
+      alt: "Whisking matcha at home",
+    },
+    {
+      src: "/homepage/5.png",
+      alt: "Serving matcha and coffee with love",
+    },
+  ],
+];
 
+const berryDiffBoxes = [
+  {
+    title: "Premium Matcha, Always",
+    description:
+      "We only serve the best! Ceremonial grade matcha that is smooth, vibrant, and whisked fresh for every cup.",
+  },
+  {
+    title: "Flavours you won't forget",
+    description:
+      "We go beyond basic! From strawberry to banana bread. Our drinks are fun, refreshing, and creative.",
+  },
+  {
+    title: "More than just a drink",
+    description:
+      "We want to deliver an experience, that you love and we love, whisked to perfection right before your eyes.",
+  },
+  {
+    title: "A Matcha Cart like no other",
+    description:
+      "We're mobile, magical, and full of flavour! We bring bold sips and good vibes to markets and events near you.",
+  },
+];
 
-
-
-export default function HomePage(): React.JSX.Element {
+export default function HomePage() {
   return (
-    <div className="flex flex-col w-full h-full">
-      <div className="grid grid-cols-4 grid-rows-2 gap-0">
-        <img src="/homepage/Image.png" alt="Image 1" className="w-full h-full object-cover" />
-        <img src="/homepage/Image2.png" alt="Image 2" className="w-full h-full object-cover" />
-        <img src="/homepage/Image3.png" alt="Image 3" className="w-full h-full object-cover" />
-        <img src="/homepage/Image4.png" alt="Image 4" className="w-full h-full object-cover" />
-        <img src="/homepage/Image8.png" alt="Image 5" className="w-full h-full object-cover" />
-        <img src="/homepage/Image7.png" alt="Image 6" className="w-full h-full object-cover" />
-        <img src="/homepage/Image6.png" alt="Image 7" className="w-full h-full object-cover" />
-        <img src="/homepage/Image5.png" alt="Image 8" className="w-full h-full object-cover" />
+    <section className="flex flex-col w-full h-full">
+      <div className="-z-10">
+        <div className="flex w-full h-90 justify-center">
+          {heroCollage[0].map((img, _) => {
+            return (
+              <img
+                key={_}
+                src={img.src}
+                alt={img.alt}
+                className="object-cover"
+              />
+            );
+          })}
+        </div>
+        <div className="flex w-full h-90 justify-center">
+          {heroCollage[1].map((img, _) => {
+            return (
+              <img
+                key={_}
+                src={img.src}
+                alt={img.alt}
+                className="object-cover"
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className="text-center bg-white-500 h-[300px]">
-        {/* This is the text below the grid */}
-        <section className={`text-[40px] text-[#262626] text-center leading-tight pt-7 font-semibold ${playfair.className}`}>
-          Serving 
-          <span style={{ color: '#25995C' }}> Matcha </span> 
-          and 
-          <span style={{color: '#77380C'}}> Coffee </span>
-          with Love,<br /> and on the go.
-        </section>
 
-        {/* little Description */}
-        <p className={`
-          text-[20px] 
-          text-[#333333]      /* Slightly lighter than #262626 for softer contrast */
-          text-center 
-          leading-tight       /* Tighter line height for neatness */
-          pt-4                /* Slightly less top padding */
-          font-semibold       /* Slightly less heavy than extrabold */
-          italic
-          ${playfair.className}
-        `}>
-          House of Berry isn’t just about coffee or matcha. It’s about turning <br />
-          everyday moments into something special—one sip at a time.
+      <div className="text-center py-12 leading-tight space-y-4 w-2xl justify-center mx-auto font-semibold text-xl">
+        <div className="text-[2.5rem] text-hob-darkgray text-center leading-tight">
+          <p>
+            Serving
+            <span className="text-hob-matcha font-script font-medium text-[3rem]">
+              {" Matcha "}
+            </span>
+            and
+            <span className="text-[#77380C] font-script font-medium text-[3rem]">
+              {" Coffee "}
+            </span>
+            with Love, and on the go.
+          </p>
+        </div>
+
+        <p className="italic">
+          House of Berry isn&apos;t just about coffee or matcha. It&apos;s about
+          turning everyday moments into something special—one sip at a time.
         </p>
-
-
-        {/* Now the Button */}
-        <button
-        className={clsx(
-          'bg-[#ECAFC0] text-black',
-          'py-2.5 px-6 leading-6',
-          'rounded-[1.1rem]',
-          'font-semibold tracking-wide',
-          'cursor-pointer',
-          'inline-flex items-center justify-center',
-          'relative-shadow',
-          'mt-7',
-          // The Hoverstate
-          'transition',
-          'hover:bg-[#F6CBD7] hover:shadow-md'
-        )}>
-          <text className={`${playfair.className}`}>Offerings</text>
-        </button>
+        <Button className="bg-hob-pink text-black rounded-xl px-6 cursor-pointer hover:bg-[#F6CBD7] hover:shadow-md mt-4 tracking-wide">
+          <Link href="/offerings" className="text-lg">
+            Offerings
+          </Link>
+        </Button>
       </div>
 
-      <div className=" bg-[#FFEFFB] h-[590px] text-center gap-4 p-4">
-        {/* heading for this container */}
-        <h4 className={
-          `text-[#ECAFC0]
-          font-bold
-          text-[33px]
-          pt-12
-          ${playfair.className}`}>
+      <div className="bg-[#FFEFFB] text-center gap-4 py-12">
+        <h1 className="text-hob-darkgray text-6xl font-script">
           The Berry Difference
-        </h4>
-
-          {/* small description about what is different */}
-        <p className={`
-        text-[#262626]
-        pt-4
-          ${playfair.className}`}>
-          Matcha made fun, flavour made bold, and moments <br />
-          made memorable. We’re not just whisking drinks - we’re <br />
-           shaking up expectations. Here’s what makes us <br />
-            different...
-        </p>
-        <div className="flex justify-center gap-4 pt-8">
-          <div className="w-[272px] h-[284px] flex flex-col items-center justify-between bg-white rounded-md p-4 shadow-lg">
-            {/* Icon or image box */}
-            <div className="w-40 h-40 bg-[#F5F5F5] rounded-md" />
-
-            {/* Subtitle */}
-            <h5 className={`text-[#25995C] text-[15px] text-lg font-semibold ${playfair.className}`}>
-              Premium Matcha, Always
-            </h5>
-
-            {/* Description */}
-            <p className={`text-[#262626] text-center text-[13px] text-[#262626] font-[500] ${playfair.className}`}>
-              We only serve the best! Ceremonial <br />
-              grade matcha that is smooth, vibrant, <br />
-              and whisked fresh for every cup.
-            </p>
-          </div>
-
-          <div className="w-[272px] h-[284px] flex flex-col items-center justify-between bg-white rounded-md p-4 shadow-lg">
-            {/* Icon or image box */}
-            <div className="w-40 h-40 bg-[#F5F5F5] rounded-md" />
-
-            {/* Subtitle */}
-            <h5 className={`text-[#25995C] text-[15px] text-lg font-semibold ${playfair.className}`}>
-              Flavours you won’t forget
-            </h5>
-
-            {/* Description */}
-            <p className={`text-[#262626] text-center text-[13px] text-[#262626] font-[500] ${playfair.className}`}>
-              We go beyond basic! From strawberry to <br />
-               banana bread. Our drinks are fun,<br />
-                refreshing, and creative.
-            </p>
-          </div>
-          <div className="w-[272px] h-[284px] flex flex-col items-center justify-between bg-white rounded-md p-4 shadow-lg">
-            {/* Icon or image box */}
-            <div className="w-40 h-40 bg-[#F5F5F5] rounded-md" />
-
-            {/* Subtitle */}
-            <h5 className={`text-[#25995C] text-[15px] text-lg font-semibold ${playfair.className}`}>
-              More than just a drink
-            </h5>
-
-            {/* Description */}
-            <p className={`text-[#262626] text-center text-[13px] text-[#262626] font-[500] ${playfair.className}`}>
-              We want to deliver an experience, that <br />
-               you love and we love, whisked to <br />
-                perfection right before your eyes.
-            </p>
-          </div>
-          <div className="w-[272px] h-[284px] flex flex-col items-center justify-between bg-white rounded-md p-4 shadow-lg">
-            {/* Icon or image box */}
-            <div className="w-40 h-40 bg-[#F5F5F5] rounded-md" />
-
-            {/* Subtitle */}
-            <h5 className={`text-[#25995C] text-[15px] text-lg font-semibold ${playfair.className}`}>
-              A Matcha Cart like no other
-            </h5>
-
-            {/* Description */}
-            <p className={`text-[#262626] text-center text-[13px] text-[#262626] font-[500] ${playfair.className}`}>
-              We’re mobile, magical, and full of <br />
-               flavour! We bring bold sips and good <br />
-                vibes to markets and events near you.
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex-row text-center justify-center h-[1390px]">
-        <h1 className={`text-[#ECAFC0] text-[40px] font-bold pt-10 ${playfair.className}`}>
-          Matcha
         </h1>
-        {/* box 1 */}
-        <div className="border w-auto h-[400px] mt-5 p-4">
-          <div className="flex justify-between gap-5">
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md padding-center">Left Box</div>
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md">
-            <img src="/matcha/image%202.png" alt="matcha-1" />
-            </div>
-          </div>
+
+        <p className="text-hob-darkgray pt-4">
+          Matcha made fun, flavour made bold, and moments <br />
+          made memorable. We&apos;re not just whisking drinks - we&apos;re
+          <br />
+          shaking up expectations. Here&apos;s what makes us different...
+        </p>
+        <div className="flex justify-center gap-6 mt-8">
+          {berryDiffBoxes.map((box, _) => {
+            return (
+              <div
+                key={_}
+                className="size-72 flex flex-col items-center justify-between bg-white rounded-md p-4 shadow-lg"
+              >
+                <div className="size-40 bg-[#F5F5F5] rounded-md" />
+                <h1 className="text-hob-matcha text-base font-semibold">
+                  {box.title}
+                </h1>
+
+                <p className="text-hob-darkgray text-center text-sm">
+                  {box.description}
+                </p>
+              </div>
+            );
+          })}
         </div>
-
-        {/* box 2 */}
-        <div className="border w-auto h-[400px] mt-5 p-4">
-          <div className="flex justify-between gap-5">
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md">
-            <img src="/matcha/image%203.png" alt="matcha-2" />
-            </div>
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md"> Right Box</div>
-          </div>
-        </div>
-
-        {/* box 3 */}
-        <div className="border w-auto h-[400px] mt-5 p-4">
-          <div className="flex justify-between gap-5">
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md">Left Box</div>
-            <div className="flex items-center justify-center bg-white w-1/2 h-[365px] rounded-md">
-            <img src="/matcha/image%204.png" alt="matcha-3" />
-            </div>
-          </div>
-        </div>
-
-
-
       </div>
-      <div className="flex-1 border p-4">Container 4</div>
-    </div>
+
+      {/* TODO: use high-res images */}
+      <div className="grid text-left px-20 py-30 gap-6 w-[115rem] mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="w-112 justify-center mx-auto">
+            <h1 className="font-modern-serif text-hob-matcha text-3xl font-bold mb-4">
+              What is Matcha?
+            </h1>
+            Matcha is a finely ground powder made from specially grown and
+            processed green tea leaves. Unlike regular tea where you steep and
+            discard the leaves, with matcha you consume the entire leaf,
+            maximizing its nutritional benefits and delivering a unique, rich
+            flavor experience.
+          </div>
+          <img src="/matcha/1.png" alt="Matcha 1" />
+        </div>
+        <div className="flex items-center justify-between">
+          <img src="/matcha/2.png" alt="Matcha 2" />
+          <div className="w-112 justify-center mx-auto  text-right">
+            <h1 className="font-modern-serif text-hob-matcha text-3xl font-bold mb-4">
+              Our Commitment to Quality
+            </h1>
+            We use ceremonial grade matcha. which is the highest quality
+            available, made from the youngest tea leaves with stems and veins
+            removed. This creates a smoother, naturally sweeter flavor without
+            bitterness. It&apos;s traditionally used in Japanese tea ceremonies
+            and offers the most vibrant colour and delicate taste experience.
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="w-112 justify-center mx-auto">
+            <h1 className="font-modern-serif text-hob-matcha text-3xl font-bold mb-4">
+              Benefits in Every Sip
+            </h1>
+            Matcha is a green powerhouse. It is rich in antioxidants, packed
+            with vitamins, and loaded with feel-good perks. It boosts energy
+            (without the crash), supports focus, aids digestion, and helps you
+            glow from the inside out
+          </div>
+          <img src="/matcha/3.png" alt="Matcha 3" />
+        </div>
+      </div>
+    </section>
   );
 }
-
-
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
-
