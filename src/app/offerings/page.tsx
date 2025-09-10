f66acf4643f16dbfb26dcb0b52aa12f0ad36d812
ghxstling <dcho282@aucklanduni.ac.nz>
2025-09-10 20:46:57 +1200

    git: fix merge

--- src/app/offerings/page.tsx
+++ src/app/offerings/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function OfferingsPage() {
-export default function OfferingsPage() {
   return (
     <section className="min-h-screen bg-hob-lightpink flex flex-col gap-4 items-center py-12 justify-center w-full max-h-fit">
       <h1 className="text-7xl font-script">Our Offerings</h1>
