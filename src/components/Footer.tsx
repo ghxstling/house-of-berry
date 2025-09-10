4bb9a2d260b0c56278c3b6c2f46bff1d836ef866
Dylan Lim <dylanlim2035@gmail.com>
2025-09-10 20:28:07 +1200

    added footer and header for mobile version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,35 @@ const LINK_STYLE = "hover:text-hob-pink transition-colors duration-100";
 
 export default function Footer() {
   return (
-    <footer className="bg-hob-lightpink lg:h-32 flex items-center w-full relative font-semibold">
-      <div className="text-center absolute left-0 right-0">
+    <footer className="bg-hob-lightpink sm:h-32 flex items-center w-full relative font-semibold">
+      <div className="hidden lg:flex text-center absolute left-0 right-0 ">
+        <div className = "">
+          <p>House of Berry</p>
+          <p>Copyright &copy; 2025</p>
+
+        </div>
+        
+        <div className="grid justify-end text-right underline top-0 right-10 absolute">
+          <Link
+            href={"https://www.instagram.com/houseofberrynz/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={LINK_STYLE}
+          >
+            <p>Instagram</p>
+          </Link>
+          <Link
+            href={"https://www.tiktok.com/@houseofberrynz"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={LINK_STYLE}
+          >
+            <p>TikTok</p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="lg:hidden text-center absolute left-0 right-0">
         <p>House of Berry</p>
         <p>Copyright &copy; 2025</p>
         <div className="grid justify-end text-right underline top-0 right-10 absolute">
@@ -29,5 +56,9 @@ export default function Footer() {
         </div>
       </div>
     </footer>
+
+    
+    
+
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuRadioGroup,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button";
+
+
 const LINK_STYLE = "hover:text-hob-pink transition-colors duration-100";
 
 export default function Header() {
+  
+  
   return (
     <header className="flex items-center justify-between lg:py-4 lg:px-12 bg-hob-lightpink shadow-sm">
       <div className="relative w-30 h-16">
@@ -17,7 +32,7 @@ export default function Header() {
         />
       </div>
 
-      <nav className="flex lg:space-x-8 text-gray-800 font-semibold">
+      <nav className="hidden lg:flex lg:space-x-8 text-gray-800 font-semibold">
         <Link href="/" className={LINK_STYLE}>
           HOME
         </Link>
@@ -34,6 +49,28 @@ export default function Header() {
           CONTACT
         </Link>
       </nav>
+      
+      <nav className="lg:hidden md:space-x-8 sm:space-x-8 lg:space-x-8 px-10 text-gray-800 font-semibold">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline"> Menu </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-56 p-2">
+            <DropdownMenuRadioGroup >
+              <DropdownMenuRadioItem value="home">  <Link href="/" className={LINK_STYLE}> HOME </Link>  </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="home">  <Link href="/about" className={LINK_STYLE}> ABOUT US </Link>  </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="home">  <Link href="/offerings" className={LINK_STYLE}> OFFERINGS </Link>  </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="home">  <Link href="/faqs" className={LINK_STYLE}> FAQS </Link>  </DropdownMenuRadioItem>
+              <DropdownMenuRadioItem value="home">  <Link href="/contact" className={LINK_STYLE}> CONTACT </Link>  </DropdownMenuRadioItem>
+              
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+      </nav>
+
+      
     </header>
   );
 }
