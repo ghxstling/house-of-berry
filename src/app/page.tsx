2f182690e09b7d81255ae1b44c9bbc90eeb3c083
ghxstling <dcho282@aucklanduni.ac.nz>
2025-09-20 13:35:01 +1200

    refactor(Home): add/update image sources to various sections and adjust style across page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,35 @@ import { Button } from "@/components/ui/button";
 
 const heroCollage = [
   {
-    src: "/homepage/1.png",
+    src: "/homepage/hero/1.png",
     alt: "Lots of Love Matcha",
   },
   {
-    src: "/homepage/2.png",
+    src: "/homepage/hero/2.png",
     alt: "House of Berry Market Stand",
   },
   {
-    src: "/homepage/3.png",
+    src: "/homepage/hero/3.png",
     alt: "Love you so Matcha",
   },
   {
-    src: "/homepage/4.png",
+    src: "/homepage/hero/4.png",
     alt: "Pouring whisked matcha into cup",
   },
   {
-    src: "/homepage/8.png",
+    src: "/homepage/hero/8.png",
     alt: "House of Berry setup",
   },
   {
-    src: "/homepage/7.png",
+    src: "/homepage/hero/7.png",
     alt: "Whisked just for you, matcha bar on the go",
   },
   {
-    src: "/homepage/6.png",
+    src: "/homepage/hero/6.png",
     alt: "Whisking matcha at home",
   },
   {
-    src: "/homepage/5.png",
+    src: "/homepage/hero/5.png",
     alt: "Serving matcha and coffee with love",
   },
 ];
@@ -43,21 +43,29 @@ const berryDiffBoxes = [
     title: "Premium Matcha, Always",
     description:
       "We only serve the best! Ceremonial grade matcha that is smooth, vibrant, and whisked fresh for every cup.",
+    image: "/homepage/berry-difference/premium-matcha.jpeg",
+    alt: "Girls holding matcha drinks",
   },
   {
     title: "Flavours you won't forget",
     description:
       "We go beyond basic! From strawberry to banana bread. Our drinks are fun, refreshing, and creative.",
+    image: "/homepage/berry-difference/flavours-you-wont-forget.jpeg",
+    alt: "House of Berry stall with products on display",
   },
   {
     title: "More than just a drink",
     description:
       "We want to deliver an experience, that you love and we love, whisked to perfection right before your eyes.",
+    image: "/homepage/berry-difference/just-a-drink.jpeg",
+    alt: "A matcha drink being prepared",
   },
   {
     title: "A Matcha Cart like no other",
     description:
       "We're mobile, magical, and full of flavour! We bring bold sips and good vibes to markets and events near you.",
+    image: "/homepage/berry-difference/matcha-cart.jpeg",
+    alt: "House of Berry stall at a market",
   },
 ];
 
@@ -78,26 +86,21 @@ export default function HomePage() {
         </div>
       </div>
 
-      <div className="text-center lg:py-12 leading-tight space-y-4 lg:w-2xl justify-center mx-auto font-semibold lg:text-xl">
-        <div className="lg:text-[2.5rem] text-hob-darkgray text-center leading-tight">
-          <p>
-            Serving
-            <span className="text-hob-matcha font-script font-medium lg:text-[3rem]">
-              {" Matcha "}
-            </span>
-            and
-            <span className="text-[#77380C] font-script font-medium lg:text-[3rem]">
-              {" Coffee "}
-            </span>
-            with Love, and on the go.
-          </p>
-        </div>
-
-        <p className="italic">
-          House of Berry isn&apos;t just about coffee or matcha. It&apos;s about
-          turning everyday moments into something special—one sip at a time.
+      <div className="text-center lg:py-12 leading-tight flex flex-col gap-4 justify-center items-center font-semibold">
+        <p className="lg:text-[2.5rem] text-hob-darkgray leading-tight lg:w-lg">
+          Serving
+          <span className="text-hob-matcha font-script font-medium lg:text-[3rem]">
+            {" Matcha "}
+          </span>
+          with Love, and on the go.
         </p>
-        <Button className="bg-hob-pink text-black rounded-xl lg:px-6 cursor-pointer hover:bg-[#F6CBD7] hover:shadow-md mt-4 tracking-wide">
+
+        <p className="italic lg:w-2xl lg:text-xl">
+          House of Berry isn&apos;t just about matcha. It&apos;s about turning
+          everyday moments into something special—one sip at a time.
+        </p>
+
+        <Button className="w-fit bg-hob-pink text-black rounded-xl lg:px-6 cursor-pointer hover:bg-[#F6CBD7] hover:shadow-md mt-4 tracking-wide">
           <Link href="/offerings" className="text-lg">
             Offerings
           </Link>
@@ -126,12 +129,14 @@ export default function HomePage() {
             return (
               <div
                 key={_}
-                className="lg:size-72 flex flex-col items-center justify-between bg-white rounded-md lg:p-4 shadow-lg"
+                className="lg:max-w-100 lg:h-full flex flex-col items-center justify-between bg-white rounded-md lg:p-4 shadow-lg lg:gap-2"
               >
-                <div className="lg:size-40 bg-[#F5F5F5] rounded-md" />
-                <h1 className="text-hob-matcha lg:text-base font-semibold">
-                  {box.title}
-                </h1>
+                <img
+                  src={box.image}
+                  alt={box.alt}
+                  className="lg:size-80 object-cover rounded-md shadow-lg lg:text-base"
+                />
+                <h1 className="text-hob-matcha font-semibold">{box.title}</h1>
 
                 <p className="text-hob-darkgray text-center lg:text-sm">
                   {box.description}
@@ -142,17 +147,16 @@ export default function HomePage() {
         </div>
       </div>
 
-      {/* TODO: use high-res images */}
       <div className="lg:py-8 flex flex-col gap-8 w-4/5 lg:max-w-screen-2xl mx-auto lg:px-12">
         <h1 className="col-span-2 font-modern-serif text-hob-pink lg:text-5xl font-bold text-center">
           MATCHA 101
         </h1>
         <div className="grid grid-cols-2 grid-rows-3 mx-auto place-items-center">
-          <div className="lg:w-112 justify-center mx-auto">
+          <div className="lg:w-112 justify-center mx-auto lg:text-xl">
             <h1 className="font-modern-serif text-hob-matcha lg:text-3xl font-bold lg:mb-4">
-              WHAT IS{" "}
+              WHAT IS
               <span className="text-hob-matcha font-script font-medium lg:text-[3rem]">
-                Matcha?
+                {" Matcha?"}
               </span>
             </h1>
             Matcha is a finely ground powder made from specially grown and
@@ -161,11 +165,17 @@ export default function HomePage() {
             maximizing its nutritional benefits and delivering a unique, rich
             flavor experience.
           </div>
-          <img src="/matcha/1.png" alt="Matcha 1" />
-          <img src="/matcha/2.png" alt="Matcha 2" />
-          <div className="lg:w-112 justify-center mx-auto">
+          <img
+            src="/homepage/matcha-101/what-is-matcha.JPG"
+            alt="Pouring matcha into cup"
+          />
+          <img src="/homepage/matcha-101/quality.JPG" alt="Prepping matcha" />
+          <div className="lg:w-112 justify-center mx-auto lg:text-xl">
             <h1 className="font-modern-serif text-hob-matcha lg:text-3xl font-bold lg:mb-4">
-              Our Commitment to Quality
+              Our Commitment to
+              <span className="text-hob-matcha font-script font-medium lg:text-[3rem]">
+                {" Quality"}
+              </span>
             </h1>
             We use ceremonial grade matcha. which is the highest quality
             available, made from the youngest tea leaves with stems and veins
@@ -173,16 +183,19 @@ export default function HomePage() {
             bitterness. It&apos;s traditionally used in Japanese tea ceremonies
             and offers the most vibrant colour and delicate taste experience.
           </div>
-          <div className="lg:w-112 justify-center mx-auto">
+          <div className="lg:w-112 justify-center mx-auto lg:text-xl">
             <h1 className="font-modern-serif text-hob-matcha lg:text-3xl font-bold lg:mb-4">
-              Benefits in Every Sip
+              <span className="text-hob-matcha font-script font-medium lg:text-[3rem]">
+                {"Benefits "}
+              </span>
+              in Every Sip
             </h1>
             Matcha is a green powerhouse. It is rich in antioxidants, packed
             with vitamins, and loaded with feel-good perks. It boosts energy
             (without the crash), supports focus, aids digestion, and helps you
             glow from the inside out.
           </div>
-          <img src="/matcha/1.png" alt="Matcha 1" />
+          <img src="/homepage/matcha-101/benefits.JPG" alt="Serving matcha" />
         </div>
       </div>
     </section>
