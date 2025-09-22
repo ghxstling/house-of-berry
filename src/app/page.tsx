503428bdedb739e9fc72550d4230827c0275028a
ghxstling <dcho282@aucklanduni.ac.nz>
2025-09-22 13:17:29 +1200

    feat(Home): add Contact Us section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import ContactForm from "@/components/ContactForm";
 
 const heroCollage = [
   {
@@ -229,6 +230,36 @@ export default function HomePage() {
           Follow the fun, one post at a time.
           <br />
           The drinks are cute. The vibes? Even cuter!
+        </p>
+      </div>
+
+      <div className="lg:gap-12 lg:py-12 grid justify-items-center bg-white">
+        <div className="lg:text-lg lg:space-y-6 text-center lg:w-4xl">
+          <h1 className="text-4xl lg:mb-12 text-hob-pink font-bold">
+            Catch the Cart
+          </h1>
+          <p>Got an event coming up? We&apos;d love to be there!</p>
+          <p>
+            Fill out the form below and our team will get back to you ASAP.
+            Whether it&apos;s a market, birthday,
+            <br />
+            wedding, or corporate function, House of Berry brings the fun
+            experience with a tailored menu to you!
+          </p>
+          <p>
+            Feel free to share a little about your event, the vibe you&apos;re
+            going for, and any questions you have.
+            <br />
+            We&apos;ll get back to you as soon as possible and take it from
+            there!
+          </p>
+        </div>
+        <div className="lg:w-3xl">
+          <ContactForm />
+        </div>
+        <p className="text-center italic">
+          If you have any other questions, feel free to <br />
+          email houseofberrynz@gmail.com
         </p>
       </div>
     </section>
